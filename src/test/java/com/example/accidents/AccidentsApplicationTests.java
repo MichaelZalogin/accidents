6325e77b5ba47d2e5d0812f47b7dd95c1f8732fa
MichaelZalogin <zaloginmiha@gmail.com>
2023-06-01 00:35:18 +0300

    Init repo accidents

---
+++ src/test/java/com/example/accidents/AccidentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccidentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
