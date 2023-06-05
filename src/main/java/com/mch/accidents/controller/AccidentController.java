ddc7e590ae32d60623980ede446d8bf609bea841
MichaelZalogin <zaloginmiha@gmail.com>
2023-06-05 23:43:03 +0300

    Рефакторинг форм, поправил отображение списков

--- src/main/java/com/mch/accidents/controller/AccidentController.java
+++ src/main/java/com/mch/accidents/controller/AccidentController.java
@@ -7,21 +7,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("accident")
 public class AccidentController {
 
     private final AccidentService accidents;
 
-    @GetMapping("/addAccident")
+    @GetMapping("/add")
     public String viewCreateAccident() {
         return "addAccident";
     }
 
-    @PostMapping("/addAccident")
+    @PostMapping("/add")
     public String save(@ModelAttribute Accident accident) {
         accidents.create(accident);
         return "redirect:/index";
     }
+
+//    @GetMapping("/edit")
+//    public String viewCreateAccident() {
+//        return "editAccident";
+//    }
+//
+//    @PostMapping("/edit")
+//    public String save(@ModelAttribute Accident accident) {
+//        accidents.edit(accident);
+//        return "redirect:/index";
+//    }
+
 }
