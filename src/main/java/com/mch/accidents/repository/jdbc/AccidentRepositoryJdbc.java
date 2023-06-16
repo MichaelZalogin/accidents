5c0b382f655d0a7c3fd650612221ea0aa43eeeb8
MichaelZalogin <zaloginmiha@gmail.com>
2023-06-16 22:57:13 +0300

    Hibernate реализации [#305522]

--- src/main/java/com/mch/accidents/repository/jdbc/AccidentRepositoryJdbc.java
+++ src/main/java/com/mch/accidents/repository/jdbc/AccidentRepositoryJdbc.java
@@ -25,6 +25,15 @@ public class AccidentRepositoryJdbc implements AccidentRepository {
         return accident;
     }
 
+    public Accident create2(Accident accident) {
+        jdbc.update("""
+                        INSERT INTO accident (name)
+                        VALUES (?)
+                        """,
+                accident.getName());
+        return accident;
+    }
+
     @Override
     public boolean update(Accident accident) {
         return jdbc.update(
